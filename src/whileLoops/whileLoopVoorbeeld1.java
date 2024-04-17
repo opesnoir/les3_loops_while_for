769a8a5163ebd8a8ba4acfa4fa76b5fcc7ce1eee
opesnoir <goodsurroundsme@outlook.com>
2024-04-17 11:22:19 +0200

    feat: while loop example

---
+++ src/whileLoops/whileLoopVoorbeeld1.java
@@ -0,0 +1,25 @@
+package whileLoops;
+
+import java.util.Scanner;
+
+public class whileLoopVoorbeeld1 {
+    public static void main(String[] args) {
+        //user input opzetten
+        Scanner input = new Scanner(System.in);
+        boolean songIsOnRepeat = true;
+        while (songIsOnRepeat){
+            // communiceer met gebruiker
+            System.out.println("song is on repeat");
+            System.out.println("do you want this song off of repeat");
+            String userInput = input.next();
+
+            // if statement om te checken of de user het nummer van repeat wil verwijderen, en de boolean dus op false moet
+            if (userInput.equals("yess")){
+                songIsOnRepeat = false;
+            }
+             }
+        System.out.println("we took the song of repeat, playing next song");
+
+
+    }
+}
